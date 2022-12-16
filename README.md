453139922e01499a19129419f20a1909d3b352fa
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-16 05:35:26 +0100

    Initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meteoapp
+
+Version 1.0.0
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.app}>
+      <h1>Hello Meteoapp</h1>
+    </div>
+  )
+}
+
+export default App
