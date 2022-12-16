127b81018794a4a3c596882c1b1d16ac65518ec2
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-16 07:32:18 +0100

    Initial search city component setup

--- src/views/SearchCity/SearchCity.jsx
+++ src/views/SearchCity/SearchCity.jsx
@@ -1,32 +1,53 @@
+import { useState, useEffect } from 'react'
+
+// third party libraries
+import { nanoid } from 'nanoid'
+
 // local components
 import Sidebar from '../../components/Sidebar/Sidebar'
+import Autocomplete from '../../components/Autocomplete/Autocomplete'
+import axios from 'axios'
 
 // local assets
 import styles from './SearchCity.module.css'
 
 const SearchCity = () => {
+
+    const [cities, setCities] = useState([])
+    const [selectedCity, setSelectedCity] = useState(null)
+    const [favorites, setFavorites] = useState([])
+
+    function handleCitySelect(city) {
+        setSelectedCity(city)
+    }
+
+    function handleFavorite(option) {
+        if (favorites.find(item => item.name === option)) return setFavorites(favorites.filter(item => item.name !== option))
+        setFavorites([...favorites, { id: nanoid(), name: option }])
+    }
+
+    useEffect(() => {
+        async function fetchCities() {
+            const response = await axios.get('/gradovi.json')
+            setCities(response.data)
+        }
+        fetchCities()
+    }, [])
+
     return (
         <div className={styles.SearchCity}>
             <Sidebar
                 title="Favorites"
-                favorites={[
-                    {
-                        id: 1,
-                        name: 'London'
-                    },
-                    {
-                        id: 2,
-                        name: 'Paris'
-                    },
-                    {
-                        id: 3,
-                        name: 'New York'
-                    }
-                ]}
-                onSelect={() => { }}
+                favorites={favorites}
+                onSelect={() => { handleCitySelect }}
             />
             <main>
-
+                <Autocomplete
+                    options={cities}
+                    onSelect={handleCitySelect}
+                    favorites={favorites}
+                    onFavorite={handleFavorite}
+                />
             </main>
         </div>
     )
