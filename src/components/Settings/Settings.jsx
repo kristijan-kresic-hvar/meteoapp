56c09d8855fcac45071c23e2eaff9b5d90c15cbd
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-18 07:43:24 +0100

    Added button that reverts settings to default state on click

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -7,7 +7,7 @@ import styles from './Settings.module.css'
 
 const Settings = () => {
 
-    const { settings, setSettings } = useContext(SettingsContext)
+    const { settings, setSettings, resetSettings } = useContext(SettingsContext)
     const { removeAllFavorites } = useContext(FavoritesContext)
 
     const handleChange = (event) => {
@@ -222,6 +222,7 @@ const Settings = () => {
                 <div className="flex flex-col sm:flex-row justify-between max-w-[380px] mx-auto gap-3">
                     <button
                         type="button"
+                        onClick={resetSettings}
                         className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                     >
                         Revert settings to default
