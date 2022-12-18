4895a8c410ac32c22c8d8cce5667223f972e76ce
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-18 07:48:08 +0100

    Edited settings state to be saved to local storage on change

--- src/context/settingsContext.jsx
+++ src/context/settingsContext.jsx
@@ -1,16 +1,19 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 const SettingsContext = createContext()
 
 const SettingsProvider = ({ children }) => {
 
-    const [settings, setSettings] = useState({
-        temperatureUnit: 'celsius',
-        windSpeedUnit: 'km/h',
-        precipitationUnit: 'milimeter',
-        timezone: 'UTC',
-        pastDays: '0',
-    })
+    const [settings, setSettings] = useState(
+        JSON.parse(localStorage.getItem('settings')) ||
+        {
+            temperatureUnit: 'celsius',
+            windSpeedUnit: 'km/h',
+            precipitationUnit: 'milimeter',
+            timezone: 'UTC',
+            pastDays: '0',
+        }
+    )
 
     const resetSettings = () => {
         setSettings({
@@ -21,6 +24,10 @@ const SettingsProvider = ({ children }) => {
             pastDays: '0',
         })
     }
+
+    useEffect(() => {
+        localStorage.setItem('settings', JSON.stringify(settings))
+    }, [settings])
 
     return (
         <SettingsContext.Provider value={{ settings, setSettings, resetSettings }}>
