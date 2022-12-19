7ca9158e6a59404df163dbcec9cafc303a6c9c3c
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-19 11:14:59 +0100

    Initial hourly filters component setup

---
+++ src/components/HourlyFilters/HourlyFilters.jsx
@@ -0,0 +1,11 @@
+import styles from './HourlyFilters.module.css'
+
+const HourlyFilters = () => {
+    return (
+        <div className={`${styles.hourlyFilters}`}>
+            HourlyFilters
+        </div>
+    )
+}
+
+export default HourlyFilters
