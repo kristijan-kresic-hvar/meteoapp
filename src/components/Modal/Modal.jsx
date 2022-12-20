de690b42f7a2cdffe90a0ee541b52f531e737a73
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-20 06:10:11 +0100

    Fixed bugs with z-indexes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -16,7 +16,7 @@ const Modal = ({ isOpen, children, onClose }) => {
                 <div
                     className={`
                     fixed top-0 bottom-0 left-0 right-0 bg-black/[.6]
-                    flex items-center justify-center`
+                    flex items-center justify-center z-[100]`
                     }
                     onClick={handleOverlayClick}
                 >
